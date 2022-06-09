b478618f736a613619584d22e21d2445c5462af8
HperM- <48154253+TitoRahman@users.noreply.github.com>
2022-06-09 15:44:15 +0700

    Added Routing

--- server/index.js
+++ server/index.js
@@ -2,54 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-
-const EmployeeModel = require('./models/Employee');
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
-
+const PORT = 3001
+const uri = process.env.ATLAS_URI
 mongoose.connect(
-    'mongodb+srv://admin:admin@cluster0.hhsrdzp.mongodb.net/Employee?retryWrites=tr' +
-            'ue&w=majority',
-    {useNewUrlParser: true}
-);
-
-app.post('/AddEmployee', async (req, res) => {
-    const FirstName = req.body.FirstName;
-    const LastName = req.body.LastName;
-    const Email = req.body.Email;
-    const Address = req.body.Address;
-    const Phone = req.body.Phone;
-
-    const employee = new EmployeeModel(
-        {FirstName: FirstName, LastName: LastName, Email: Email, Address: Address, Phone: Phone}
-    );
-
-    try {
-        await employee.save();
-        res.send("inserted data");
-    } catch (err) {
-        console.log(err);
+    uri, {
+        useNewUrlParser: true
     }
-});
+);
+mongoose.connection.once('open', () => {
+    console.log('Connected to DB')
+})
 
-app.get('/viewAllEmployee', async (req, res) => {
-    EmployeeModel.find({}, (err, data) => {
-        if (err) {
-            res.send(err);
-        }
-        res.send(data);
-    });
-});
-
-app.get('/viewSingleEmployee/:id', async (req, res) => {
-    const id = req.params.id;
-    EmployeeModel.find({id}, (err, data) => {
-        if (err) {
-            res.send(err);
-        }
-        res.send(data);
-    });
-});
-
-app.listen(3001, () => console.log('Listening on port 3001'));
+const employee = require('./routes/employee');
+app.use('/api', employee)
+app.listen(PORT, () => console.log('Listening on port 3001'));
